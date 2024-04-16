c120810d53e22d7dbf0215ec9f9ed24017eab045
Ovchinnikov Alexey <gdhald4@gmail.com>
2024-04-16 21:17:44 +0500

    employee add buttons

--- PROJECT BUILD/employeeForm.Designer.cs
+++ PROJECT BUILD/employeeForm.Designer.cs
@@ -30,16 +30,25 @@
         {
             label1 = new Label();
             dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Unispace", 15.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Microsoft Sans Serif", 15.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.Location = new Point(12, 9);
             label1.Name = "label1";
-            label1.Size = new Size(210, 25);
+            label1.Size = new Size(204, 25);
             label1.TabIndex = 2;
             label1.Text = "Наши сотрудники";
             // 
@@ -48,21 +57,110 @@
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.AllowUserToDeleteRows = false;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 46);
+            dataGridView1.Location = new Point(12, 51);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
-            dataGridView1.Size = new Size(730, 562);
+            dataGridView1.Size = new Size(968, 562);
             dataGridView1.TabIndex = 3;
+            dataGridView1.SelectionChanged += dataGridView1_SelectionChanged_1;
             // 
-            // employee
+            // textBox1
+            // 
+            textBox1.Location = new Point(254, 619);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(155, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(254, 648);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(155, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(254, 677);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(155, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(254, 706);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(155, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tahoma", 12F);
+            label2.Location = new Point(210, 618);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 19);
+            label2.TabIndex = 8;
+            label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 12F);
+            label3.Location = new Point(174, 647);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 19);
+            label3.TabIndex = 9;
+            label3.Text = "Фамилия";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Tahoma", 12F);
+            label4.Location = new Point(157, 677);
+            label4.Name = "label4";
+            label4.Size = new Size(91, 19);
+            label4.TabIndex = 10;
+            label4.Text = "Должность";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Tahoma", 12F);
+            label5.Location = new Point(171, 706);
+            label5.Name = "label5";
+            label5.Size = new Size(77, 19);
+            label5.TabIndex = 11;
+            label5.Text = "Зарплата";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Tahoma", 12F, FontStyle.Bold);
+            button1.Location = new Point(436, 699);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 30);
+            button1.TabIndex = 12;
+            button1.Text = "Изменить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // employeeForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.WhiteSmoke;
-            ClientSize = new Size(754, 620);
+            ClientSize = new Size(992, 762);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(dataGridView1);
             Controls.Add(label1);
-            Name = "employee";
+            Name = "employeeForm";
             Text = "Сотрудники";
             Load += employee_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -74,5 +172,14 @@
 
         private Label label1;
         private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
     }
 }
--- PROJECT BUILD/employeeForm.cs
+++ PROJECT BUILD/employeeForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PROJECT_BUILD;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,7 @@ namespace PROJECT_BUILD
     public partial class employeeForm : Form
     {
         Database database = new Database();
+
         public employeeForm()
         {
             InitializeComponent();
@@ -33,7 +35,7 @@ namespace PROJECT_BUILD
             dataGridView1.Columns.Add("id", "ID");
             dataGridView1.Columns.Add("name", "Имя");
             dataGridView1.Columns.Add("second_name", "Фамилия");
-            dataGridView1.Columns.Add("job_title", "Должность"); 
+            dataGridView1.Columns.Add("job_title", "Должность");
             dataGridView1.Columns.Add("date", "Дата приема");
             dataGridView1.Columns.Add("Salary", "Зарплата");
             dataGridView1.Columns.Add("Department_ID", "ID отдела");
@@ -41,21 +43,21 @@ namespace PROJECT_BUILD
             dataGridView1.Columns.Add("IsNew", String.Empty);
         }
 
-        
-            private void ReadSinglRows(DataGridView dgw, IDataRecord record)
-            {
-                dgw.Rows.Add(
-                    record.GetInt32(0),
-                    record.GetString(1),
-                    record.GetString(2),
-                    record.GetString(3),
-                    record.GetDateTime(4).ToString("yyyy-MM-dd"), // Преобразуем дату в строку с нужным форматом
-                    record.IsDBNull(5) ? "NULL" : record.GetDecimal(5).ToString(), // Проверяем, не является ли значение NULL
-                    record.IsDBNull(6) ? "NULL" : record.GetInt32(6).ToString() // Проверяем, не является ли значение NULL
-                );
-            }
 
-        
+        private void ReadSinglRows(DataGridView dgw, IDataRecord record)
+        {
+            dgw.Rows.Add(
+                record.GetInt32(0),
+                record.GetString(1),
+                record.GetString(2),
+                record.GetString(3),
+                record.GetDateTime(4).ToString("yyyy-MM-dd"),
+                record.IsDBNull(5) ? "NULL" : record.GetDecimal(5).ToString(),
+                record.IsDBNull(6) ? "NULL" : record.GetInt32(6).ToString()
+            );
+        }
+
+
 
         private void UpdateDataGridView(DataGridView dgw)
         {
@@ -71,6 +73,64 @@ namespace PROJECT_BUILD
             reader.Close();
         }
 
-        
+        private void dataGridView1_SelectionChanged_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                textBox1.Text = null;
+                textBox2.Text = null;
+                textBox3.Text = null;
+                textBox4.Text = null;
+
+
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                string field1Value = selectedRow.Cells[1].Value.ToString();
+                string field2Value = selectedRow.Cells[2].Value.ToString();
+                string field3Value = selectedRow.Cells[3].Value.ToString();
+                string field4Value = selectedRow.Cells[5].Value.ToString();
+                textBox1.Text = field1Value;
+                textBox2.Text = field2Value;
+                textBox3.Text = field3Value;
+                textBox4.Text = field4Value;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newFieldValue1 = textBox1.Text;
+            string newFieldValue2 = textBox2.Text;
+            string newFieldValue3 = textBox3.Text;
+            string newFieldValue4 = textBox4.Text;
+
+            
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                selectedRow.Cells[1].Value = newFieldValue1;
+                selectedRow.Cells[2].Value = newFieldValue2;
+                selectedRow.Cells[3].Value = newFieldValue3;
+                selectedRow.Cells[5].Value = newFieldValue4;
+
+                UpdateDataInDatabase(selectedRow.Cells[0].Value.ToString(), newFieldValue1, newFieldValue2, newFieldValue3, newFieldValue4);
+            }
+        }
+
+        private void UpdateDataInDatabase(string id, string newValue1, string newValue2, string newValue3, string newValue4)
+        {
+            // Предполагается, что newValue4 - это числовое значение, например, decimal или int
+            string updateQuery = $"UPDATE dbo.Сотрудники SET Имя = '{newValue1}', Фамилия = '{newValue2}', Должность = '{newValue3}', Зарплата = '{newValue4}' WHERE ID = {id}";
+            SqlCommand command = new SqlCommand(updateQuery, database.GetConnection());
+            database.openConnection();
+            command.ExecuteNonQuery();
+            database.closedConnection();
+        }
+
     }
+
 }
+
+
+
+
